399b74fb523e401548c8dc38329a179b751a4564
AndroidHell <ralkkai1337@gmail.com>
2015-07-06 21:25:38 -0500

    Finsh setup for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,14 @@
 class Listing < ActiveRecord::Base
+
+	if rails.env.develop?
+			  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
 	  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
 	  
 										  :storage => :dropbox,
-									    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+									    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+									    :path => ":style/:id_filename"
 									    
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  end
 end
